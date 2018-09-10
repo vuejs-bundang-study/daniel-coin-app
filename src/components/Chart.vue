cf0a4c9b95181afe1efa99ea95ff2ae18a35be21
ahribori <ahribori@gmail.com>
2018-09-11 00:58:26 +0900

    업비트 실시간 코인 시세 한눈에 보기 구현.

---
+++ src/components/Chart.vue
@@ -0,0 +1,98 @@
+<template>
+    <v-card v-model="coinType">
+        <div id="container"></div>
+    </v-card>
+</template>
+
+<script>
+    import axios from 'axios';
+    import Highcharts from 'highcharts/highstock';
+
+    export default {
+        name: 'Chart',
+        props: {
+            coinType: String,
+            coinSelected: Object,
+        },
+        watch: {
+            coinSelected: function (coin) {
+                if (this.chart) {
+                    const lastDataIndex = this.chart.series[1].data.length - 1;
+                    const prevPrice = this.chart.series[1].data[lastDataIndex].y;
+                    const currentPrice = coin.tradePrice;
+                    if (prevPrice !== currentPrice) {
+                        console.log(prevPrice);
+                        this.chart.series[1].data[lastDataIndex].update({ y: coin.tradePrice }, true, false);
+                        console.log(this.chart.series[1].data[lastDataIndex].y);
+                        console.log('-------');
+                    }
+                }
+            },
+        },
+        methods: {
+            fetchData: function (coinType) {
+                if (coinType) {
+                    return axios.get(`https://crix-api-endpoint.upbit.com/v1/crix/candles/days?code=${coinType}&count=180`);
+                }
+            },
+            initializeChart: function (data) {
+                this.chart = Highcharts.stockChart('container', {
+                    rangeSelector: {
+                        selected: 1,
+                    },
+
+                    title: {
+                        text: this.coinType,
+                    },
+
+                    series: [{
+                        type: 'candlestick',
+                        name: '가격정보',
+                        data: data,
+                    }],
+                });
+            },
+            clearChart: function () {
+                if (this.chart && this.chart.forExport) {
+                    this.chart.destroy();
+                }
+            },
+            chartTransaction: function () {
+                this.clearChart();
+                this.fetchData(this.coinType)
+                    .then((result) => {
+                        const chartData = result.data
+                            .sort((a, b) => {
+                                return a.timestamp - b.timestamp;
+                            })
+                            .map(data => {
+                                return [
+                                    data.timestamp,
+                                    data.openingPrice,
+                                    data.highPrice,
+                                    data.lowPrice,
+                                    data.tradePrice,
+                                ];
+                            });
+                        this.initializeChart(chartData);
+                    })
+                    .catch(() => {
+                        alert('데이터를 가져오는데 실패했습니다.');
+                    });
+            },
+        },
+        updated: function () {
+            this.chartTransaction();
+        },
+        mounted: function () {
+            this.chartTransaction();
+        },
+        beforeDestroy: function () {
+            this.clearChart();
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CoinCard.vue
@@ -0,0 +1,142 @@
+<template>
+    <div>
+        <v-card :class="{ 'selected': selected }">
+            <v-card-title primary-title>
+                <h3 class="headline mb-0">{{filterCoinName(data.code)}}</h3>
+                <Candle
+                        class="align-end"
+                        :highPrice="data.highPrice"
+                        :lowPrice="data.lowPrice"
+                        :openingPrice="data.openingPrice"
+                        :currentPrice="data.tradePrice"
+                        :direction="data.change"
+                ></Candle>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-list dense>
+                <v-list-tile :class="{
+        'rise-font': data.change === 'RISE',
+        'fall-font': data.change === 'FALL',
+        'rise': data.tradePrice - data.prevTradePrice > 0,
+        'fall': data.tradePrice - data.prevTradePrice < 0 }"
+                >
+                    <v-list-tile-content><h3>{{comma(data.tradePrice)}} 원</h3></v-list-tile-content>
+                    <v-list-tile-content class="align-end">{{changeDirection(data.change)}}{{comma(data.changePrice)}}원
+                        ({{data.change === 'RISE' ? '+' : '-'}}{{Number(data.changeRate * 100).toFixed(2)}}%)
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                    <v-list-tile-content>시가</v-list-tile-content>
+                    <v-list-tile-content class="align-end">{{comma(data.openingPrice)}} 원</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                    <v-list-tile-content>고가</v-list-tile-content>
+                    <v-list-tile-content class="align-end">{{comma(data.highPrice)}} 원</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                    <v-list-tile-content>저가</v-list-tile-content>
+                    <v-list-tile-content class="align-end">{{comma(data.lowPrice)}} 원</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                    <v-list-tile-content>거래대금</v-list-tile-content>
+                    <v-list-tile-content class="align-end">{{accTradePrice24h}} 백만원
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-card>
+    </div>
+</template>
+
+<script>
+    import Candle from './Candle';
+
+    export default {
+        name: 'CoinCard',
+        props: {
+            data: Object,
+            selected: Boolean,
+        },
+        computed: {
+            accTradePrice24h: function () {
+                const value = this.data.accTradePrice24h;
+                return this.comma((value / 1000000).toFixed(0));
+            },
+        },
+        methods: {
+            filterCoinName(coinName) {
+                if (!coinName) {
+                    return;
+                }
+                return coinName.replace('CRIX.UPBIT.KRW-', '');
+            },
+            comma(x) {
+                if (!x) {
+                    return null;
+                }
+                return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            },
+            changeDirection(change) {
+                switch (change) {
+                    case 'RISE': {
+                        return '↑';
+                    }
+                    case 'FALL': {
+                        return '↓';
+                    }
+                    default:
+                        return null;
+                }
+            },
+        },
+        components: {
+            Candle,
+        },
+    };
+</script>
+
+<style scoped>
+    .selected {
+        box-sizing: border-box;
+        border: 1px dashed gray;
+    }
+
+    .rise {
+        animation: rise 1s;
+    }
+
+    .fall {
+        animation: fall 1s;
+    }
+
+    .rise-font {
+        color: red;
+    }
+
+    .fall-font {
+        color: darkblue;
+    }
+
+    @keyframes rise {
+        0% {
+            bacground-color: rgba(0, 0, 255, 0);
+        }
+        50% {
+            background-color: rgba(0, 0, 255, 0.1);
+        }
+        100% {
+            bacground-color: rgba(0, 0, 255, 0);
+        }
+    }
+
+    @keyframes fall {
+        0% {
+            bacground-color: rgba(255, 0, 0, 0);
+        }
+        50% {
+            background-color: rgba(255, 0, 0, 0.1);
+        }
+        100% {
+            bacground-color: rgba(255, 0, 0, 0);
+        }
+    }
+</style>
